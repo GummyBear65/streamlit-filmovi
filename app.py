dc6bc1e74e33e568c8b09f299d5a00d5d53cf485
GummyBear65 <105936084+GummyBear65@users.noreply.github.com>
2025-11-21 18:55:05 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,63 +1,56 @@
 import streamlit as st
 import pandas as pd
-from ucitaj import ucitaj_podatke
+from dohvat_podataka import dohvat_podataka
 
 SHEET_URL = st.secrets["sheet_url"]
 SHEET_NAME = "filmovi"
-df, worksheet = ucitaj_podatke(SHEET_URL, SHEET_NAME)
-
+#==============================================================================
+df, worksheet = dohvat_podataka(SHEET_URL,SHEET_NAME)
 df["Godina"] = pd.to_numeric(df["Godina"])
 df["Ocjena"] = pd.to_numeric(df["Ocjena"])
-
-st.title("Omiljeni filmovi")
-
+#==============================================================================
+st.title("Moji omiljeni filmovi")
 st.subheader("Trenutni popis filmova")
 st.dataframe(df)
-#poglavlje za dodavanje filmova
+#==============================================================================
 st.subheader("Dodaj novi film")
 naslov = st.text_input("Naslov")
-godina = st.number_input("Godina", step=1, format="%d")
+godina = st.number_input("Godina", step=1 ,format="%d") #format="%d" - cijeli broj
 zanr = st.text_input("Žanr")
-ocjena = st.slider("Ocjena", 1, 10)
-
+ocjena = st.slider("Ocjena",1,10)
+#==============================================================================
 if st.button("Dodaj film"):
-    novi_red = [naslov, int(godina), zanr, ocjena]
+    novi_red = [naslov,int(godina),zanr,ocjena]
     worksheet.append_row(novi_red)
-    st.success("Film je uspješno dodan!")
+    st.success(F"Film je uspješno dodan")
     st.rerun()
-#poglavlje za pretragu filmova
-st.subheader("Pretrazi filmove")
+#==============================================================================
+st.subheader("Pretraži filmove")
 filtrirani = df.copy()
 
-zanr_filter = st.text_input("Pretraži po žanru")
-godina_filter = st.number_input("Pretraži po godini", step=1, format="%d")
+žanr_filt = st.text_input("Pretraži po žanru")
+godina_filt= st.number_input("Pretraži po godini" , step = 1, format="%d")
 
-if zanr_filter:
-    filtrirani = filtrirani[filtrirani["Žanr"].str.contains(zanr_filter, case=False, na=False)]
+if žanr_filt:
+    filtrirani = filtrirani[filtrirani["Žanr"].str.contains(žanr_filt, case=False)]
 
-if godina_filter:
-    filtrirani = filtrirani[filtrirani["Godina"] == int(godina_filter)]
+if godina_filt:
+    filtrirani = filtrirani[filtrirani["Godina"] == int(godina_filt)]
 
 st.dataframe(filtrirani)
-#poglavlje za brisanje filmova
-st.subheader("Obriši film")
+#==============================================================================
+st.subheader("Brisanje filmova")
 
 filmovi_opcije = df.apply(lambda row: f"{row["Naslov"]} ({row["Godina"]})", axis=1).tolist()
 film_za_brisanje = st.selectbox("Odaberi film za brisanje", options=filmovi_opcije)
 
-if st.button("Obriši film"):
+if st.button("Izbriši film"):
     for idx, row in df.iterrows():
         if f"{row["Naslov"]} ({row["Godina"]})" == film_za_brisanje:
-            worksheet.delete_rows(idx + 2)  # +2 zbog zaglavlja i 0-indeksiranja
-            st.success("Film je uspješno obrisan!")
+            worksheet.delete_rows(idx + 2)
+            st.success(f"Film je uspješno izbrisan")
             st.rerun()
 
-#poglavlje za prikaz top 3 filmova
-st.subheader("TOP 3 FILMOVA")
-
-top3= df.sort_values(by="Ocjena", ascending=False).head(3)
+st.subheader("TOP 3 FILMA")
+top3= df.sort_values(by = "Ocjena", ascending = False).head(3)
 st.table(top3)
-
-
-            
-        
